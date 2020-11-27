5f68f0fc0ea0f3291e41761ddf6cf758c1be0b05
shashwath kr <68803494+shashwath-KR@users.noreply.github.com>
2020-11-27 19:21:00 +0530

    Add files via upload

---
+++ script15.js
@@ -0,0 +1,8 @@
+
+function myFunction() {
+	const form= document.getElementById("form").style.display= "block";
+}
+
+function onClose() {
+const form= document.getElementById("form").style.display= "none";
+}
